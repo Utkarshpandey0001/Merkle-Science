b0461c0bfb3859576e12f110e546fab9925e1877
Utkarshpandey0001 <rajutkarshpandey2003@gmail.com>
2026-09-21 22:53:25 +0530

    Complete book search and price filters

--- app/services/books.py
+++ app/services/books.py
@@ -2,7 +2,7 @@
 from typing import Optional
 
 from fastapi import HTTPException
-from sqlalchemy import select
+from sqlalchemy import or_, select
 from sqlalchemy.orm import Session
 
 from app.models import Book
@@ -65,10 +65,15 @@ def list_books(
     """
     query = select(Book)
     if q:
-        query = query.where(Book.title.icontains(q, autoescape=True))
+        query = query.where(
+            or_(Book.title.icontains(q, autoescape=True), Book.author.icontains(q, autoescape=True))
+        )
     if restricted is not None:
         query = query.where(Book.restricted == restricted)
-    # TODO: min_price / max_price filters
+    if min_price is not None:
+        query = query.where(Book.price_cents >= min_price)
+    if max_price is not None:
+        query = query.where(Book.price_cents <= max_price)
 
     # TODO: apply ``sort``
     books = db.scalars(query.order_by(Book.id.asc()).limit(limit).offset(offset)).all()
